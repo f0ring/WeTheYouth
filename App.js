215fddbf97c738d081a1be2dc7939fc1fff9fe76
Apple.com <nahin4512@gmail.com>
2025-08-23 20:59:23 +0600

    Ourstory
    Wetheyouth

---
+++ App.js
@@ -0,0 +1,14 @@
+
+// src/App.js
+import React from "react";
+import Ourstory from "./Ourstory"; 
+
+function App() {
+  return (
+    <div>
+      <Ourstory />
+    </div>
+  );
+}
+
+export default App;
